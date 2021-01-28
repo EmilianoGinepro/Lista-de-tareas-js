35bf6eb4f5c425e527fdeaa1d9ff17c86bbd8a32
sislac1986 <68289251+sislac1986@users.noreply.github.com>
2021-01-27 22:26:22 -0300

    fecha
    se agregaron las fechas de las tareas

--- Lista de tareas/app.js
+++ Lista de tareas/app.js
@@ -1,20 +1,31 @@
 //agregar una nueva tarea
 document.getElementById("btn_aceptar").addEventListener('click',() =>{    
+
 let Tarea = document.getElementById('agregar');
-let Fecha = document.getElementById('fecha');
+let tareaValor = Tarea.value;
+
+let Fecha = document.getElementById('fecha').value;
+let fechaValor = new Date(Fecha).toLocaleDateString()
+
+console.log(fechaValor);
+
+
 if(Tarea.value ==='' || Fecha.value ===''){
     return false;
 }
 
 var nuevaTarea = {
-    Tarea: Tarea.value,
-    Fecha: Fecha.value, 
+    Tarea: tareaValor,
+    Fecha: fechaValor, 
     }
-    
+    console.log(nuevaTarea);
     generarTemplate(nuevaTarea);
+    
     Tarea.value = ''
     Fecha.value = ''   
+    
 })
+
   
 //generar html y agregar el objeto
 const lista = document.querySelector('.to-dos');
@@ -28,11 +39,16 @@ const generarTemplate = tareaNueva =>{
     lista.innerHTML +=html;
 };
 
-
 //borrar tareas
 lista.addEventListener('click', e=>{
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
 })
- 
+
+/*
+   Trabajar el formato del  date
+1-Busca moment.js
+2-convierte el date en toLocaleString(), toma los 10 primeros con slice , con slipt separa lo por '/' y lo construyes a tu manera en una cadena
+3-Busca en Google los formatos que te da dateTimePicker, junto con jquery puedes establecer el formato que desees recuperar.
+*/
